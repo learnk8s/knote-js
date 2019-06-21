ffec00fd9f2364a7be1650efd2e672c0cf5a386a
Daniel Weibel <danielmweibel@gmail.com>
2019-06-22 00:42:44 +0200

    Fix and improve code

--- 01/index.js
+++ 01/index.js
@@ -6,7 +6,6 @@ const marked = require('marked')
 
 const app = express()
 const port = process.env.PORT || 3000
-const upload = multer({ dest: path.join(__dirname, 'public/uploads/') })
 const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017'
 
 async function initMongo() {
@@ -20,35 +19,33 @@ async function initMongo() {
       await new Promise(resolve => setTimeout(resolve, 1000))
     }
   }
-  const db = client.db('dev').collection('notes')
   console.log("MongoDB initialised")
-  return db
+  return client.db('dev').collection('notes')
 }
 
 async function start() {
   const db = await initMongo() 
 
   app.set('view engine', 'pug')
-  app.set('views', path.join(__dirname, '/views'))
+  app.set('views', path.join(__dirname, 'views'))
   app.use(express.static(path.join(__dirname, 'public')))
 
   app.get('/', async (req, res) => {
-    res.render('index', { notes: await getNotesAsMd(db) })
+    res.render('index', { notes: await retrieveNotes(db) })
   })
 
-  app.post('/note', upload.single('image'), async (req, res) => {
-    if (req.body.upload) {
+  app.post('/note', multer({ dest: path.join(__dirname, 'public/uploads/') }).single('image'), async (req, res) => {
+    if (!req.body.upload && req.body.description) {
+      await saveNote(db, { description: req.body.description })
+      res.redirect('/')
+    }
+    else if (req.body.upload && req.file) {
       const link = `/uploads/${encodeURIComponent(req.file.filename)}`
-      return res.render('index', {
+      res.render('index', {
         content: `${req.body.description} ![](${link})`,
-        notes: await getNotesAsMd(db),
+        notes: await retrieveNotes(db),
       })
     }
-    const description = req.body.description
-    if (!!description) {
-      await insertNote(db, { description })
-    }
-    res.redirect('/')
   })
 
   app.listen(port, () => {
@@ -56,14 +53,12 @@ async function start() {
   })
 }
 
-async function insertNote(db, note) {
-  const client = await db
-  await client.insertOne(note)
+async function saveNote(db, note) {
+  await db.insertOne(note)
 }
 
-async function getNotesAsMd(db) {
-  const client = await db
-  const notes = await client.find().toArray()
+async function retrieveNotes(db) {
+  const notes = (await db.find().toArray()).reverse()
   return notes.map(it => {
     return { ...it, description: marked(it.description) }
   })
--- 04-05/index.js
+++ 04-05/index.js
@@ -7,7 +7,6 @@ const minio = require('minio')
 
 const app = express()
 const port = process.env.PORT || 3000
-const upload = multer({ storage: multer.memoryStorage() })
 const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017'
 const minioHost = process.env.MINIO_HOST || "localhost"
 const minioBucket = 'image-storage'
@@ -23,9 +22,8 @@ async function initMongo() {
       await new Promise(resolve => setTimeout(resolve, 1000))
     }
   }
-  const db = client.db('dev').collection('notes')
   console.log("MongoDB initialised")
-  return db
+  return client.db('dev').collection('notes')
 }
 
 async function initMinIO() {
@@ -35,12 +33,12 @@ async function initMinIO() {
     port: 9000,
     useSSL: false,
     accessKey: process.env.MINIO_ACCESS_KEY,
-    secretKey: process.env.MINIO_SECRET_KEY,
+    secretKey: process.env.MINIO_SECRET_KEY
   })
   let success = false
   while (!success) {
     try {
-      if (!await client.bucketExists(minioBucket)) {
+      if (! await client.bucketExists(minioBucket)) {
         await client.makeBucket(minioBucket)
       }
       success = true
@@ -57,27 +55,26 @@ async function start() {
   const minio = await initMinIO()
 
   app.set('view engine', 'pug')
-  app.set('views', path.join(__dirname, '/views'))
+  app.set('views', path.join(__dirname, 'views'))
   app.use(express.static(path.join(__dirname, 'public')))
 
   app.get('/', async (req, res) => {
-    res.render('index', { notes: await getNotesAsMd(db) })
+    res.render('index', { notes: await retrieveNotes(db) })
   })
 
-  app.post('/note', upload.single('image'), async (req, res) => {
-    if (req.body.upload) {
+  app.post('/note',multer({ storage: multer.memoryStorage() }).single('image'), async (req, res) => {
+   if (!req.body.upload && req.body.description)  {
+      await saveNote(db, { description: req.body.description })
+      res.redirect('/')
+    }
+    else if (req.body.upload && req.file) {
       await minio.putObject(minioBucket, req.file.originalname, req.file.buffer)
       const link = `/img/${encodeURIComponent(req.file.originalname)}`
-      return res.render('index', {
+      res.render('index', {
         content: `${req.body.description} ![](${link})`,
-        notes: await getNotesAsMd(db),
+        notes: await retrieveNotes(db),
       })
     }
-    const description = req.body.description
-    if (!!description) {
-      await insertNote(db, { description })
-    }
-    res.redirect('/')
   })
 
   app.get('/img/:name', async (req, res) => {
@@ -90,14 +87,12 @@ async function start() {
   })
 }
 
-async function insertNote(db, note) {
-  const client = await db
-  await client.insertOne(note)
+async function saveNote(db, note) {
+  await db.insertOne(note)
 }
 
-async function getNotesAsMd(db) {
-  const client = await db
-  const notes = await client.find().toArray()
+async function retrieveNotes(db) {
+  const notes = (await db.find().toArray()).reverse()
   return notes.map(it => {
     return { ...it, description: marked(it.description) }
   })
